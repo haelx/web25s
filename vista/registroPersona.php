7f4a71e5e0d7f92404c4a5b4220d897a23588bda
haelx <alentandome@hotmail.com>
2024-08-02 08:49:15 -0400

    agregado de controlador

--- vista/registroPersona.php
+++ vista/registroPersona.php
@@ -8,10 +8,13 @@
     <form action="../controlador/ctrlPersona.php" method="post">
         <label for="nombres">Nombres:</label>
         <input type="text" id="nombres" name="nombres">
+        <hr>
         <label for="papellido">Primer Apellido:</label>
         <input type="text" name="papellido" id="papellido">
+        <hr>
         <label for="sapellido">Segundo Apellido:</label>
         <input type="text" id="sapellido" name="sapellido">
+        <hr>
         <button type="submit">Registrar</button>
     </form>
 </body>
