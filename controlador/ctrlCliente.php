6cd8dd461ef6a76de2a62e7eadd58f86d5145728
haelx <alentandome@hotmail.com>
2024-08-23 11:29:54 -0400

    registro Cliente

--- controlador/ctrlCliente.php
+++ controlador/ctrlCliente.php
@@ -1,41 +1,18 @@
 <?php
-print_r($_POST);
-die();
-
 include_once "../modelo/modCliente.php";
 //Llenado de los datos del formulario html.
 if($_POST) {
-    $nombre_cliente = htmlspecialchars($_POST['nombre_cliente']);
-    $nombre_contacto = htmlspecialchars($_POST['nombre_contacto']);
-    $apellido_contacto = htmlspecialchars($_POST['apellido_contacto']);
-    $telefono = htmlspecialchars($_POST['telefono']);
-    $fax = htmlspecialchars($_POST['fax']);
-    $linea_direccion1 = htmlspecialchars($_POST['linea_direccion1']);
-    $linea_direccion2 = htmlspecialchars($_POST['linea_direccion2']);
-    $ciudad = htmlspecialchars($_POST['ciudad']);
-    $region = htmlspecialchars($_POST['region']);
-    $pais = htmlspecialchars($_POST['pais']);
-    $codigo_postal = htmlspecialchars($_POST['codigo_postal']);
-    $codigo_empleado_rep_ventas = htmlspecialchars($_POST['codigo_empleado_rep_ventas']);
-    $limite_credito = htmlspecialchars($_POST['limite_credito']);
-//uso de los set del modelo cliente
-    $cliente = new Cliente();
-    $cliente->setNombreCliente($nombre_cliente);
-    $cliente->setNombreContacto($nombre_contacto);
-    $cliente->setApellidoContacto($apellido_contacto);
-    $cliente->setTelefono($telefono);
-    $cliente->setFax($fax);
-    $cliente->setLineaDireccion1($linea_direccion1);
-    $cliente->setLineaDireccion2($linea_direccion2);
-    $cliente->setCiudad($ciudad);
-    $cliente->setRegion($region);
-    $cliente->setPais($pais);
-    $cliente->setCodigoPostal($codigo_postal);
-    if ($codigo_empleado_rep_ventas == "") {
-        $codigo_empleado_rep_ventas = 'null';
-    }
-    $cliente->setCodigoEmpleadoRepVentas($codigo_empleado_rep_ventas);
-    $cliente->setLimiteCredito($limite_credito);
-    $cliente->registrarCliente();
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $apellido1 = htmlspecialchars($_POST['apellido1']);
+    $apellido2 = htmlspecialchars($_POST['apellido2']);
+    $email = htmlspecialchars($_POST['email']);
+    $direccion = htmlspecialchars($_POST['direccion']);
 
+    $modCli= new Cliente();
+    $modCli->setNombre($nombre);
+    $modCli->setApellido1($apellido1);
+    $modCli->setApellido2($apellido2);
+    $modCli->setEmail($email);
+    $modCli->setDireccion($direccion);
+    $modCli->RegistrarCliente();
 }
--- modelo/modCliente.php
+++ modelo/modCliente.php
@@ -1,325 +1,96 @@
 <?php
 include_once "conexion/conexionBase.php";
 class Cliente {
-    private $codigo_cliente;
-    private $nombre_cliente;
-    private $nombre_contacto;
-    private $apellido_contacto;
-    private $telefono;
-    private $fax;
-    private $linea_direccion1;
-    private $linea_direccion2;
-    private $ciudad;
-    private $region;
-    private $pais;
-    private $codigo_postal;
-    private $codigo_empleado_rep_ventas;
-    private $limite_credito;
+    private $idCliente;
+    private $nombre;
+    private $apellido1;
+    private $apellido2;
+    private $direccion;
+    private $email;
     private $con;
-
-    public function __construct()
-    {
-        $this->codigo_cliente=null;
-        $this->nombre_cliente="";
-        $this->nombre_contacto="";
-        $this->apellido_contacto="";
-        $this->telefono="";
-        $this->fax="";
-        $this->linea_direccion1="";
-        $this->linea_direccion2="";
-        $this->ciudad="";
-        $this->region="";
-        $this->pais="";
-        $this->codigo_postal="";
-        $this->codigo_empleado_rep_ventas='null';
-        $this->limite_credito=0.0;
-        $this->con= new ConexionBase();
+    public function __construct(){
+        $this->idCliente= null;
+        $this->nombre="";
+        $this->apellido1="";
+        $this->apellido2="";
+        $this->direccion="";
+        $this->email="";
+        $this->con=new ConexionBase();
     }
 
     /**
-     * Get the value of codigo_cliente
+     * @return null
      */
-    public function getCodigoCliente()
+    public function getIdCliente()
     {
-        return $this->codigo_cliente;
+        return $this->idCliente;
     }
 
     /**
-     * Set the value of codigo_cliente
+     * @param null $idCliente
      */
-    public function setCodigoCliente($codigo_cliente): self
+    public function setIdCliente($idCliente)
     {
-        $this->codigo_cliente = $codigo_cliente;
-
-        return $this;
+        $this->idCliente = $idCliente;
     }
 
-    /**
-     * Get the value of nombre_cliente
-     */
-    public function getNombreCliente()
+    public function getNombre(): string
     {
-        return $this->nombre_cliente;
+        return $this->nombre;
     }
 
-    /**
-     * Set the value of nombre_cliente
-     */
-    public function setNombreCliente($nombre_cliente): self
+    public function setNombre(string $nombre)
     {
-        $this->nombre_cliente = $nombre_cliente;
-
-        return $this;
+        $this->nombre = $nombre;
     }
 
-    /**
-     * Get the value of nombre_contacto
-     */
-    public function getNombreContacto()
+    public function getApellido1(): string
     {
-        return $this->nombre_contacto;
+        return $this->apellido1;
     }
 
-    /**
-     * Set the value of nombre_contacto
-     */
-    public function setNombreContacto($nombre_contacto): self
+    public function setApellido1(string $apellido1)
     {
-        $this->nombre_contacto = $nombre_contacto;
-
-        return $this;
+        $this->apellido1 = $apellido1;
     }
 
-    /**
-     * Get the value of apellido_contacto
-     */
-    public function getApellidoContacto()
+    public function getApellido2(): string
     {
-        return $this->apellido_contacto;
+        return $this->apellido2;
     }
 
-    /**
-     * Set the value of apellido_contacto
-     */
-    public function setApellidoContacto($apellido_contacto): self
+    public function setApellido2(string $apellido2)
     {
-        $this->apellido_contacto = $apellido_contacto;
-
-        return $this;
+        $this->apellido2 = $apellido2;
     }
 
-    /**
-     * Get the value of telefono
-     */
-    public function getTelefono()
+    public function getDireccion(): string
     {
-        return $this->telefono;
+        return $this->direccion;
     }
 
-    /**
-     * Set the value of telefono
-     */
-    public function setTelefono($telefono): self
+    public function setDireccion(string $direccion)
     {
-        $this->telefono = $telefono;
-
-        return $this;
+        $this->direccion = $direccion;
     }
 
-    /**
-     * Get the value of fax
-     */
-    public function getFax()
+    public function getEmail(): string
     {
-        return $this->fax;
+        return $this->email;
     }
 
-    /**
-     * Set the value of fax
-     */
-    public function setFax($fax): self
+    public function setEmail(string $email)
     {
-        $this->fax = $fax;
-
-        return $this;
+        $this->email = $email;
     }
 
-    /**
-     * Get the value of linea_direccion1
-     */
-    public function getLineaDireccion1()
+    public function RegistrarCliente()
     {
-        return $this->linea_direccion1;
-    }
-
-    /**
-     * Set the value of linea_direccion1
-     */
-    public function setLineaDireccion1($linea_direccion1): self
-    {
-        $this->linea_direccion1 = $linea_direccion1;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of linea_direccion2
-     */
-    public function getLineaDireccion2()
-    {
-        return $this->linea_direccion2;
-    }
-
-    /**
-     * Set the value of linea_direccion2
-     */
-    public function setLineaDireccion2($linea_direccion2): self
-    {
-        $this->linea_direccion2 = $linea_direccion2;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of ciudad
-     */
-    public function getCiudad()
-    {
-        return $this->ciudad;
-    }
-
-    /**
-     * Set the value of ciudad
-     */
-    public function setCiudad($ciudad): self
-    {
-        $this->ciudad = $ciudad;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of region
-     */
-    public function getRegion()
-    {
-        return $this->region;
-    }
-
-    /**
-     * Set the value of region
-     */
-    public function setRegion($region): self
-    {
-        $this->region = $region;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of pais
-     */
-    public function getPais()
-    {
-        return $this->pais;
-    }
-
-    /**
-     * Set the value of pais
-     */
-    public function setPais($pais): self
-    {
-        $this->pais = $pais;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of codigo_postal
-     */
-    public function getCodigoPostal()
-    {
-        return $this->codigo_postal;
-    }
-
-    /**
-     * Set the value of codigo_postal
-     */
-    public function setCodigoPostal($codigo_postal): self
-    {
-        $this->codigo_postal = $codigo_postal;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of codigo_empleado_rep_ventas
-     */
-    public function getCodigoEmpleadoRepVentas()
-    {
-        return $this->codigo_empleado_rep_ventas;
-    }
-
-    /**
-     * Set the value of codigo_empleado_rep_ventas
-     */
-    public function setCodigoEmpleadoRepVentas($codigo_empleado_rep_ventas): self
-    {
-        $this->codigo_empleado_rep_ventas = $codigo_empleado_rep_ventas;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of limite_credito
-     */
-    public function getLimiteCredito()
-    {
-        return $this->limite_credito;
-    }
-
-    /**
-     * Set the value of limite_credito
-     */
-    public function setLimiteCredito($limite_credito): self
-    {
-        $this->limite_credito = $limite_credito;
-
-        return $this;
-    }
-
-    public function registrarCliente(){
         $this->con->createConnection();
-        $sql="insert into cliente (
-        nombre_cliente,
-        nombre_contacto,
-        apellido_contacto,
-        telefono,
-        fax,
-        linea_direccion1,
-        linea_direccion2,
-        ciudad,
-        region,
-        pais,
-        codigo_postal,
-        codigo_empleado_rep_ventas,
-        limite_credito
-        ) values (
-        '$this->nombre_cliente',
-        '$this->nombre_contacto',
-        '$this->apellido_contacto',
-        '$this->telefono',
-        '$this->fax',
-        '$this->linea_direccion1',
-        '$this->linea_direccion2',
-        '$this->ciudad',
-        '$this->region',
-        '$this->pais',
-        '$this->codigo_postal',
-        $this->codigo_empleado_rep_ventas,
-        '$this->limite_credito'
-        )";
+        $sql="insert into cliente(nombre, apellido1, apellido2, direccion, email)
+values('$this->nombre', '$this->apellido1', '$this->apellido2', '$this->direccion', '$this->email')";
         $this->con->executeQuery($sql);
-
+        echo "Registro exitoso";
     }
+
 }
